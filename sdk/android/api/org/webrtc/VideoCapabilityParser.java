06c35ad349b807d6fe69122f80202b441eaf804a
Andrejus <anba8005@gmail.com>
2020-11-23 19:01:36 +0200

    android build fix

--- sdk/android/api/org/webrtc/VideoCapabilityParser.java
+++ sdk/android/api/org/webrtc/VideoCapabilityParser.java
@@ -14,7 +14,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.webrtc.Logging;
 import org.xml.sax.SAXException;
 
 import java.io.File;
